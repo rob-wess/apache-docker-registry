88fea9eabfcf4179c921e55a28a324af26ee35c6
new-jack <rob.wess86@gmail.com>
2019-07-26 06:19:02 -0400

    Updated docker-prereqs

--- scripts/docker_prereqs.sh
+++ scripts/docker_prereqs.sh
@@ -2,7 +2,8 @@
 
 
 # Formatting variables
-	dot=$(systemctl status smb | head -n 1  | cut -d " " -f 1)
+	damon=$(systemctl list-unit-files | grep enabled | head -n 1 | awk '{print $1}')
+	dot=$(systemctl status $damon | head -n 1  | cut -d " " -f 1)
 	wid=$(tput cols)
 	total=$(( $wid - 23 ))
 	SUCCESS=$(tput setaf 2; tput bold; echo "SUCCESS")
@@ -18,6 +19,7 @@ function Print-Message ()
 		 echo -e "\t$3 $4 ${v// /.} $5"
 	}
 
+
 function Check-Host ()
 	{
 		tput bold; echo -e \\n"Check Host Requirements"\\n; tput sgr0
@@ -26,9 +28,11 @@ function Check-Host ()
 		message="Check ansible is installed"
 		len=$(echo $message | wc -c)
 		difference=$(( $total - $len - 7 ))
-		if [ ! -f /bin/ansible/ ]; then
+		if [ -f /bin/ansible/ ]; then
+			export ansible_installed="1"
 			Print-Message " " $difference $dot "$message" "$SUCCESS" && tput sgr0
 		else
+			export ansible_installed="0"
 			Print-Message " " $difference $dot "$message" "$FAIL" && tput sgr0
 			echo -e "\tCHECK: $message" >> $fail_log
 			echo -e "\t\tRESULT: Could not find the file /bin/ansible. Is it somewhere else?"\\n >> $fail_log
@@ -38,9 +42,11 @@ function Check-Host ()
 		message="Check docker is installed"
 		len=$(echo $message | wc -c)
 		difference=$(( $total - $len - 7 ))
-		if [ ! -f /bin/docker/ ]; then
+		if [ -f /bin/docker/ ]; then
+			export docker_installed="1"
 			Print-Message " " $difference $dot "$message" "$SUCCESS" && tput sgr0
 		else
+			export docker_installed="0"
 			Print-Message " " $difference $dot "$message" "$FAIL" && tput sgr0
 			echo -e "\tCHECK: $message" >> $fail_log
 			echo -e "\t\tRESULT: Could not find the file /bin/docker. Is it somewhere else?"\\n >> $fail_log
@@ -50,12 +56,51 @@ function Check-Host ()
 		message="Check docker-compose is installed"
 		len=$(echo $message | wc -c)
 		difference=$(( $total - $len - 7 ))
-		if [ -f /usr/local/bin/docker-compose ]; then
+		if [ ! -f /usr/local/bin/docker-compose ]; then
+			export docker_compose_installed="1"
+			Print-Message " " $difference $dot "$message" "$SUCCESS" && tput sgr0
+		else
+			export docker_compose_installed="0"
+			Print-Message " " $difference $dot "$message" "$FAIL" && tput sgr0
+			echo -e "\tCHECK: $message" >> $fail_log
+			echo -e "\t\tRESULT: Could not find the file /usr/local/bin/docker-compose. Is it somewhere else?"\\n >> $fail_log
+		fi
+
+	if [[ "$ansible_installed" == "1" && "$docker_installed" == "1" && "$docker_compose_installed" == "1" ]]; then
+		export host_status="prepped"
+	else
+		export host_status="unprepped"
+	fi
+	
+	}
+	
+function Unpack-Tars ()
+	{
+		tput bold; echo -e \\n"Untar Needed Files"\\n; tput sgr0
+		
+		message="Untar install_docker_ce role"
+		len=$(echo $message | wc -c)
+		difference=$(( $total - $len - 7 ))
+
+		if (tar -xzvf $PWD/install_docker_ce.tar.gz &> /dev/null); then
 			Print-Message " " $difference $dot "$message" "$SUCCESS" && tput sgr0
 		else
 			Print-Message " " $difference $dot "$message" "$FAIL" && tput sgr0
 			echo -e "\tCHECK: $message" >> $fail_log
-			echo -e "\t\tRESULT: Could not find the file /usr/local/bin/docker-compose. Is it somewhere else?"\\n >> $fail_log
+			echo -e "\t\tRESULT: Could not untar install_docker_ce.tar.gz"\\n >> $fail_log
+		fi
+
+		
+		message="Untar deploy_secure_registry role"
+		len=$(echo $message | wc -c)
+		difference=$(( $total - $len - 7 ))
+			
+		if (tar -xzvf $PWD/deploy_secure_registry.tar.gz &> /dev/null); then
+			Print-Message " " $difference $dot "$message" "$SUCCESS" && tput sgr0
+		else
+			Print-Message " " $difference $dot "$message" "$FAIL" && tput sgr0
+			echo -e "\tCHECK: $message" >> $fail_log
+			echo -e "\t\tRESULT: Could not untar deploy_secure_registry.tar.gz"\\n >> $fail_log
 		fi
 	}
 
@@ -96,18 +141,35 @@ echo > $fail_log
 
 #Run the Check-Host function and exit if any fails
 Check-Host 
-tput bold; tput setaf 2; tput smul
-echo -e \\n"ALL CHECKS HAVE PASSED"
-tput sgr0
-echo -e \\n"The system is ready for installation and deployment of the secured registry container."\\n
-tput sgr0
-read -p "Press enter to continue or Ctrl+c to exit"
+if [[ "$host_status" == "prepped" ]]; then
+	tput bold; tput setaf 2; tput smul
+	echo -e \\n"ALL CHECKS HAVE PASSED"
+	tput sgr0
+	echo -e \\n"The system is ready for installation and deployment of the secured registry container."\\n
+	tput sgr0
+	read -p "Press enter to continue or Ctrl+c to exit"
+else
+	tput bold; tput setaf 1; tput smul
+	echo -e \\n"Host does not have all pre-reqs installed"
+	tput sgr0
+	echo -e \\n"The system is not ready for installation or deployment of the secured registry container."\\n
+	tput sgr0
+	echo -e \\n"We'll call the installer scripts now"\\n
+	read -p "Press enter to continue or Ctrl+c to exit"
+	
+		scripts/install_prereqs.sh
+	
+fi
+exit
 
-#Sudo up
-sudo bash
+#Unpack tar files
+Unpack-Tars
+exit
+
 
 # Call the openssl function 
 Call-Openssl
+
 # Call the Ansible-Plays function
 Ansible-Plays
 
