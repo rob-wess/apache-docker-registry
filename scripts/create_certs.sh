db04ec6c84462fb3eac7798c8de318661e813ff5
rob-wess <53195588+rob-wess@users.noreply.github.com>
2019-08-01 18:44:01 -0400

    Variablized subject for certs

--- scripts/create_certs.sh
+++ scripts/create_certs.sh
@@ -2,30 +2,14 @@
 
 dockerDir=/home/docker/docker-registry
 
-echo $subj
-exit
-
-
-
 if [ ! -d $dockerDir ]; then
-	mkdir -p $dockerDir
+        mkdir -p $dockerDir
 fi
 
 rm -fv $dockerDir/*.{key,crt,csr,cer}
 
-
-echo "Step 1"
-echo "openssl genrsa -out $dockerDir/devdockerCA.key 2048" # && clear
-
-echo "Step 2"
-echo "openssl req -x509 -new -nodes -key $dockerDir/devdockerCA.key -days 10000 -subj=$subj -out $dockerDir/devdockerCA.crt" # && clear
-
-echo "Step 3"
-echo "openssl genrsa -out $dockerDir/domain.key 2048" #&& clear
-
-echo "Step 4"
-echo "openssl req -subj=$subj -new -key $dockerDir/domain.key -out $dockerDir/dev-docker-registry.com.csr" # && clear
-
-echo "Step 5"
-echo "openssl x509 -req -in $dockerDir/dev-docker-registry.com.csr -CA $dockerDir/devdockerCA.crt -CAkey $dockerDir/devdockerCA.key -CAcreateserial -out $dockerDir/domain.crt -days 10000"
-exit
+openssl genrsa -out $dockerDir/devdockerCA.key 2048
+openssl req -x509 -new -nodes -key $dockerDir/devdockerCA.key -days 10000 -subj "$subj" -out $dockerDir/devdockerCA.crt
+openssl genrsa -out $dockerDir/domain.key 2048
+openssl req -subj $subj -new -key $dockerDir/domain.key -out $dockerDir/dev-docker-registry.com.csr
+openssl x509 -req -in $dockerDir/dev-docker-registry.com.csr -CA $dockerDir/devdockerCA.crt -CAkey $dockerDir/devdockerCA.key -CAcreateserial -out $dockerDir/domain.crt -days 10000
